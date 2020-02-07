e164e0810145acc2f2725a366dabf2638ce3845e
jasondavindev <jasondavin.dev@gmail.com>
2020-02-07 14:56:12 -0300

    add localstack

--- workers/services/aws.js
+++
@@ -1,6 +0,0 @@
-const AWS = require('aws-sdk');
-
-const credentials = new AWS.SharedIniFileCredentials();
-AWS.config.update({ credentials, region: process.env.AWS_REGION });
-
-module.exports = AWS;
--- workers/services/sqs.js
+++ workers/services/sqs.js
@@ -1,10 +1,16 @@
-const AwsService = require('./aws');
+const AWS = require('aws-sdk');
 
 class SQSService {
-  constructor(url) {
-    this.service = new AwsService.SQS();
-    this.url = url;
+  constructor(options = {}) {
+    this.service = new AWS.SQS(options);
+    this.url = options.url;
   }
 }
 
-module.exports = new SQSService(process.env.SQS_URL);
+module.exports = new SQSService({
+  endpoint: process.env.AWS_SQS_ENDPOINT,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  url: process.env.AWS_SQS_URL,
+});
